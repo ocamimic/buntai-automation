9d79b0c0766d909acc53618df9fa6bdd1a5dc77d
ocami <66419581+ocamimic@users.noreply.github.com>
2025-02-05 11:32:32 +0900

    Update main.py
    コメントを挿入した。

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import gspread
 from google.oauth2.service_account import Credentials
 import random
 import numpy
-#スプレッドシートのリンク
-URL = ''
+
+URL = input("スプレッドシートのURL: ")
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
 ]
-#ローカルで書いてるときの名残
 credentials = Credentials.from_service_account_file(
     'python/gss/gss_credential.json',
     scopes=scopes
@@ -17,29 +16,32 @@ credentials = Credentials.from_service_account_file(
 gc = gspread.authorize(credentials)
 sheet = gc.open_by_url(URL)
 ansSheet = sheet.worksheet("フォームの回答 1")
-value = ansSheet.get_all_values()[1:]#リストとして回答データを取得
+rawValue = ansSheet.get_all_values()#リストとして回答データを取得。
+value = rawValue[1:]#質問のタイトル部分を消去
 rowCount = len(value)#回答の数
-colCount = len(value[0])#質問の数
+colCount = len(rawValue[0])#質問の数
 
-contentIndex = numpy.argmax(value[0])
-print(contentIndex)
+contentIndex = numpy.argmax(value[0])#もっとも回答が長いところを本文として取得
+
 i = 0
-randNum = []
+randNum = []#作品ごとに乱数を与えるための空リスト
 while i < rowCount:
     randNum.append(random.random())
     i += 1
-shuffled = [x for _,x in sorted(zip(randNum,value))]
-template = ['[[tab No.', ']]\n', '\n[[/tab]]\n', '----', '[[include]]']
+shuffled = [x for _,x in sorted(zip(randNum,value))]#ランダムな順番に並べ替え。shuffledの値はそれぞれ[0:乱数,1:著者名…contentIndex:本文]となる
+
 output = '[[tabview]]\n'
 authorAns = []
+#それぞれ出力用の枠を初期化。
 j = 0
 while j < rowCount:
-    output += template[0] + str(j + 1) + template[1] + value[j][contentIndex] + template[2]
-    authorAns.append(value[1])
+    output += '[[tab No.' + str(j + 1) + ']]\n' + shuffled[j][contentIndex] + '\n[[/tab]]\n'
+    authorAns.append('\nNo.' + str(j + 1) + ': ' + str(shuffled[j][1]))
     j += 1
 output += '[[/tabview]]\n'
-file = open('文体集計出力.txt', 'a')
-#未完成。テキストファイルで出力する。
-#参加者リスト、本文を格納したタブ、番号と著者の対応表
-file.write(output)
-file.write(template[3])
+file = open('python/gss/文体集計出力.txt', 'a', encoding='UTF-8')
+toWrite = ['本文ココから↓\n', output, '==========\n本文ココまで\n著者回答ココから↓']
+file.writelines(toWrite)
+file.writelines(authorAns)
+file.close
+print('出力完了！')
